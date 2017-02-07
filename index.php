c52483b2f901833990f7de65b12059348b92d518
Rochelle Lewis <rlewis37@cnm.edu>
2017-02-07 15:50:12 -0700

    added basic html and css - ready to style

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<title>CSS Sandbox</title>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<h1>CSS Sandbox</h1>
+			</div>
+		</header>
+
+		<main>
+			<div class="container">
+				<div class="row">
+					<div class="">
+						<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.</p>
+					</div>
+					<div class="">
+						<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="">
+						<img class="float-left" src="http://www.fillmurray.com/200/300" alt="fill murray">
+						<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.</p>
+					</div>
+				</div>
+			</div>
+		</main>
+	</body>
+</html>
